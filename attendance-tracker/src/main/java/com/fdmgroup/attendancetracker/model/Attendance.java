78fd574a05e74b45ecb391f7bb7ce8a7d93b692b
edgar.afonso <edgar.afonso@fdmgroup.com>
2023-01-28 17:35:05 +0000

    Updated App.js to display Hello World Message.
    Removed unused Imports on:
    * Attendance model class
    * Trainee model class

--- attendance-tracker/src/main/java/com/fdmgroup/attendancetracker/model/Attendance.java
+++ attendance-tracker/src/main/java/com/fdmgroup/attendancetracker/model/Attendance.java
@@ -2,7 +2,6 @@ package com.fdmgroup.attendancetracker.model;
 
 import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
